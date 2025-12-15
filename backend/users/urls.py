62ec864fc576feec040093b0571d6be8997994fb
Nazrawi Gedion <119290537+nazrawigedion123@users.noreply.github.com>
2025-12-15 11:43:04 +0300

    Revert "google auth implmeneted"

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,5 @@
-# backend/users/urls.py
-from django.urls import path,include
-from .views import CreateUserView, CustomTokenObtainPairView, google_callback
-from . import views
+from django.urls import path
+from .views import CreateUserView, CustomTokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 
@@ -9,10 +7,4 @@ urlpatterns = [
     path('register/', CreateUserView.as_view(), name='register'),
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
-    path('auth/', include('allauth.socialaccount.providers.google.urls')),
-    path('auth/', include('dj_rest_auth.urls')),
-    path('auth/registration/', include('dj_rest_auth.registration.urls')),
-
-    path('auth/google/callback/success/', views.google_callback, name='google_auth_success'),
 ]
